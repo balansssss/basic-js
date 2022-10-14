d42cc238d2c9c0a203d9fe268eb09dfacc7a6675
Balanchuk Sergey <balansstudio1@gmail.com>
2022-10-14 21:24:46 +0300

    feat: add encode-task solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let lett = null;
+  let lettCount = 1;
+  let res = '';
+
+  function getCount(count) {
+    return count > 1 ? count : '';
+  }
+
+  if (str.length === 1 || str === '') return str;
+
+  for (let i = 0; i < str.length; i++) {
+    if (i === 0) {
+      lett = str[i];
+    } else if (str[i] === lett) {
+      lettCount += 1;
+    } else {
+      res += getCount(lettCount) + lett;
+      lett = str[i];
+      lettCount = 1;
+    }
+  }
+  return res += getCount(lettCount) + lett;
 }
 
 module.exports = {
